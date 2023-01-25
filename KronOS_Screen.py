b1a2b3caaa7b0b16a95a8ad7b7794286fa11bcea
Cavolik <101195373+Cavolik@users.noreply.github.com>
2023-01-25 16:13:04 +0100

    Update KronOS_Screen.py

--- KronOS_Screen.py
+++ KronOS_Screen.py
@@ -1,15 +1,49 @@
 #KronOS Screen 1.0 Base code
-#TO DO: Useless loading screen, Change screen
+#TO DO: Change screen
 import time
 import configparser
 from luma.lcd.device import hd44780
 from luma.core.interface.parallel import bitbang_6800
 
-config = configparser.ConfigParser()
-
 #Set screen
 interface = bitbang_6800(RS=11, E=17, PINS=[27, 22, 10, 9])
 device = hd44780(interface, width=20, height=4)
+
+
+l1 = "X                  X"
+l2 = "   Loading KronOS   "
+l3 = "  <>                "
+l4 = "X                  X"
+device.text = '\n'.join([l1, l2, l3, l4])
+l3 = "  <>"
+time.sleep(1)
+device.text = '\n'.join([l1, l2, l3, l4])
+l3 = "  <==>"
+time.sleep(1)
+device.text = '\n'.join([l1, l2, l3, l4])
+l3 = "  <====>"
+time.sleep(1)
+device.text = '\n'.join([l1, l2, l3, l4])
+l3 = "  <======>"
+time.sleep(1)
+device.text = '\n'.join([l1, l2, l3, l4])
+l3 = "  <========>"
+time.sleep(1)
+device.text = '\n'.join([l1, l2, l3, l4])
+l3 = "  <==========>"
+time.sleep(1)
+device.text = '\n'.join([l1, l2, l3, l4])
+l3 = "  <============>"
+time.sleep(1)
+device.text = '\n'.join([l1, l2, l3, l4])
+l3 = "  <==============>"
+time.sleep(1)
+device.text = '\n'.join([l1, l2, l3, l4])
+l3 = "  <================>"
+time.sleep(2.4)
+
+
+config = configparser.ConfigParser()
 
 while True:
     for I in range (7):
@@ -19,8 +53,8 @@ while True:
 
         #Print data
         l1 = config["ALARM"]["lin1"]
-        l2 = config["ALARM"]["lin2"]
-        l3 = "Now playing:"
+        l2 = config["ALARM"]["lin2"] 
+        l3 = "Now playing: "
         l4 = config["RADIO"]["playing"]
         device.text = '\n'.join([l1, l2, l3, l4])
         time.sleep(1)
