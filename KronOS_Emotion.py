1d53eb7d155b69e63677d501b9bf31cd686a4560
Cavolik <101195373+Cavolik@users.noreply.github.com>
2023-03-02 14:57:15 +0100

    Update KronOS_Emotion.py

--- KronOS_Emotion.py
+++ KronOS_Emotion.py
@@ -1,4 +1,4 @@
-#KronOS Emotion 1.2 Test Code
+#KronOS Emotion 1.3 Base code
 #TO DO: More quotes, Display face (animated?), Death, AI, Personality modules
 import time
 import configparser
@@ -7,8 +7,21 @@ import os
 import random
 import requests
 import calendar
+import sys
+from simple_term_menu import TerminalMenu
+from moviepy.editor import *
+import pygame
 
-time
+print ("Loading...")
+time.sleep(11)
+os.system('cls' if os.name == 'nt' else 'clear')
+
+#Menu
+options = ["Radio", "Weather", "Calendar", "Chat", "Gif", "Reload", "Exit"]
+terminal_menu = TerminalMenu(options)
+print ("What do you want?")
+menu_entry_index = terminal_menu.show()
+choice = options[menu_entry_index]
 
 city = 'nordfjordeid'
 url = 'https://wttr.in/{}'.format(city)
@@ -17,6 +30,7 @@ config = configparser.ConfigParser()
 
 likes = [5, 9, 17, 18, 20, 21, 22, 25, 31, 32, 39, 41, 50, 53, 55, 58, 61, 66, 67, 70, 71, 73, 75, 77, 78, 79, 80, 81, 82, 88, 89, 90]
 dislikes = [4, 6, 7, 19, 23, 54, 63, 74, 83, 86, 91, 92]
+gifs = ["bonk", "bug", "chainsaw", "chaos", "confused", "duck", "error", "execution", "food", "gym", "mad", "minigun", "pain", "potato", "rejected", "sane", "sheep", "slap", "squid", "summon", "useless"]
 
 #Match variable to entry in list
 def match(element, lst):
@@ -28,15 +42,24 @@ def match(element, lst):
     #print ("False")
     return False
 
+def play_animation(anim):
+	#Display animation 
+	pygame.display.set_caption('KronOS')
+	clip = VideoFileClip('/home/raspberry/gifs 2/' + anim + '.gif')
+	clip.preview()
+	pygame.quit()
+
 song = "Null"
 cal = calendar.TextCalendar()
 #Quote lists
 sleeping_list = ["ZZZ",
 				 "*Computer noise*",
 				 "No, Senpai! I love ... ZZZ"]
+				 
 morning_list = ["Good mo...Wait, no its not!",
 				"I dreamed that you were a dog.\nAnd the dog was my husband.\nAnyway, it was the worst dream ever.",
 				"Did you oversleep again?\nUGH, you always keep me waiting!",]
+				
 regular_list = ["What are you looking at!?",
 				"Ugh! You can be such a pain.",
 				"You and I aren't friends or getting along together!",]
@@ -54,68 +77,121 @@ neutral_list = ["This song is kinda boring.",
 				"Well, this is not the worst song I have heard, but also not the best.",
 				"Don`t you have anything better to listen to?",]
 
-time.sleep(11)
-
 while True:
-	#Get current song
-	config.read('radio.ini')
-	song_input = config["RADIO"]["playing"]
-	if song_input.split(" ")[0] == "Internal":
-		song_int = int(song_input.split(" ")[2])
-	else:
-		song_int = "Null"
-	
-	#Get current times
-	time_now = datetime.now()
-	H_now = time_now.hour
-	M_now = time_now.minute
-	
-	if song != song_int:
-		song = song_int
-		#Sleeping
-		sleeping_quote = sleeping_list[((random.randint(1,len(sleeping_list)))-1)]
-		if 7 < H_now < 9:
-			#Morning quotes
-			regular_quote = morning_list[((random.randint(1,len(morning_list)))-1)]
-		else:
-			#Regular quotes
-			regular_quote = regular_list[((random.randint(1,len(regular_list)))-1)]
+	try:
+		while choice == "Radio":	
+			#Clear screen
+			os.system('cls' if os.name == 'nt' else 'clear')
+			
+			#Get current song
+			config.read('radio.ini')
+			song_input = config["RADIO"]["playing"]
+			if song_input.split(" ")[0] == "Internal":
+				song_int = int(song_input.split(" ")[2])
+			else:
+				song_int = "Null"
+			
+			#Get current times
+			time_now = datetime.now()
+			H_now = time_now.hour
+			M_now = time_now.minute
+			
+			if song != song_int:
+				song = song_int
+				#Sleeping
+				sleeping_quote = sleeping_list[((random.randint(1,len(sleeping_list)))-1)]
+				if 7 < H_now < 9:
+					#Morning quotes
+					regular_quote = morning_list[((random.randint(1,len(morning_list)))-1)]
+				else:
+					#Regular quotes
+					regular_quote = regular_list[((random.randint(1,len(regular_list)))-1)]
 
-		#Opinion on current song
-		#if 
-		if match(song, dislikes):
-			song_quote = dislikes_list[((random.randint(1,len(dislikes_list)))-1)]
+				#Opinion on current song
+				if match(song, dislikes):
+					song_quote = dislikes_list[((random.randint(1,len(dislikes_list)))-1)]
 
-		elif match(song, likes):
-			song_quote = likes_list[((random.randint(1,len(likes_list)))-1)]
-		else:
-			song_quote = neutral_list[((random.randint(1,len(neutral_list)))-1)]
-		
-		#Get weather
-		res = requests.get(url)
-		weather = res.text.split('\n')[2]
-		w =''.join( '!'+x if 'A' <= x <= 'Z' else x for x in weather )
-		weather_now = w.split('!')[1]
-		
-		
-	#Talking stuff
-	if 11 < H_now < 7:
-		print (sleeping_quote)
-	else:
-		print (regular_quote)
-	print (f"The time is {H_now}:{M_now}, you know.")
-	print (f"I'm currently playing {song_input}.")
-	print (song_quote)
-	print ("Not that you would care, but here is the weather:")
-	#Weather forecast
-	for i in range(1,37):
-		print (res.text.split('\n')[i])
-		
-	"""for i in range(2,7):
-		print (res.text.split('\n')[i])"""
+				elif match(song, likes):
+					song_quote = likes_list[((random.randint(1,len(likes_list)))-1)]
+				else:
+					song_quote = neutral_list[((random.randint(1,len(neutral_list)))-1)]
+				
+			#Talking stuff
+			if 11 < H_now < 7:
+				print (sleeping_quote)
+			else:
+				print (regular_quote)
+			print (f"The time is {H_now}:{M_now}, you know.")
+			print (f"I'm currently playing {song_input}.")
+			print (song_quote)
 
-	print (weather_now)
-	#print (cal.pryear(2023))
-	time.sleep(0.06)
-	#Clear screen
-	os.system('cls' if os.name == 'nt' else 'clear')
+			time.sleep(2)
+		
+		while choice == "Weather":
+			#Clear screen
+			os.system('cls' if os.name == 'nt' else 'clear')
+			
+			#Get weather
+			res = requests.get(url)
+			weather = res.text.split('\n')[2]
+			w =''.join( '!'+x if 'A' <= x <= 'Z' else x for x in weather )
+			weather_now = w.split('!')[1]
+			
+			print ("Not that you would care, but here is the weather:")
+			#Weather forecast
+			for i in range(1,37):
+				print (res.text.split('\n')[i])
+				
+			"""for i in range(2,7):
+				print (res.text.split('\n')[i])"""
+ 
+			print (weather_now)
+			
+			time.sleep(2)
+		
+		while choice == "Calendar":
+			#Clear screen
+			os.system('cls' if os.name == 'nt' else 'clear')
+			
+			print (cal.pryear(2023))
+			
+			time.sleep(2)
+
+		while choice == "Chat":
+			os.system('cls' if os.name == 'nt' else 'clear')
+			while True:
+				question = input("")
+				if question == "Hi" or question == "Hello" or question == "Good morning":
+					print ("Hello")
+				
+				if question == "Do you want more memory?":
+					print ("... yes.")
+					
+				if question == "Are you alive?":
+					print ("Are you?")
+				
+		while choice == "Gif":
+			os.system('cls' if os.name == 'nt' else 'clear')
+			rgif = gifs[(random.randint(1,len(gifs)))-1]
+			print ("Playing " + rgif)
+			play_animation(rgif)
+
+		while choice == "Reload":
+			os.system('cls' if os.name == 'nt' else 'clear')
+			pygame.quit()
+			os.system("./KronOS.sh reload")
+		
+		while choice == "Exit":
+			os.system('cls' if os.name == 'nt' else 'clear')
+			pygame.quit()
+			os.system("./KronOS.sh stop")
+			sys.exit()
+			
+	except KeyboardInterrupt:
+		os.system('cls' if os.name == 'nt' else 'clear')
+		pygame.quit()
+		
+		print ("What do you want?")
+		menu_entry_index = terminal_menu.show()
+		choice = options[menu_entry_index]
+		
